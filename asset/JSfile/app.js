380fc1eb4f64dc85306619dd6dcef4f97de8cd3d
Sumit-sarkar <sumitopticalit89@gmail.com>
2025-07-27 02:31:19 +0600

    20% complete

--- asset/JSfile/app.js
+++ asset/JSfile/app.js
@@ -1,14 +1,19 @@
 const openMenu = document.querySelector(".openMenu");
 const closeMenu = document.querySelector(".closeMenu");
 const menu = document.querySelector("#menu");
+const navigation = document.querySelector("#navigation");
 
 openMenu.addEventListener("click", () => {
     openMenu.style.display = "none";
     closeMenu.style.display = "block";
     menu.style.boxShadow = "rgba(255, 217, 0, 0.267) 0px -2px 10px";
+    navigation.style.height = "80vh";
+    
 });
 closeMenu.addEventListener("click", () => {
     openMenu.style.display = "flex";
     closeMenu.style.display = "none";
      menu.style.boxShadow = "none";
+     navigation.style.height = "0vh";
+     
 });
